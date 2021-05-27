c7e0a00a45594ad75e4e46d808dacd06f06a6e19
ParadigmZero <josephrcalton@gmail.com>
2021-05-27 14:53:03 +0100

    Previously done code, added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
+import React, { useState } from 'react';
+import { uploadFile } from 'react-s3';
+
+const config = {
+  bucketName: '',
+  dirName: '',
+  region: '',
+  accessKeyId: '',
+  secretAccessKey: '',
+};
+
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const [isSelected, setIsSelected] = useState(false);
+
+
+  const browseClick = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const uploadClick = () => {
+
+    config.dirName = Date.now();
+
+    uploadFile(selectedFile, config)
+      .then((data) => {
+        console.log(`https://${config.bucketName}.s3.${config.region}.amazonaws.com/${config.dirName}/${selectedFile.name}`);
+      })
+      .catch(
+        (err) => { alert(err) }
+      )
+
+    setIsSelected(false);
+    setSelectedFile();
+
+  }
+
+
+
+
+
   return (
-    <>
-      Simple
-    </>
+    <div>
+      <input type="file" name="file" onChange={browseClick} title="" />
+      {isSelected ? (
+        <div>
+
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={uploadClick}>Submit</button>
+      </div>
+    </div>
   );
+
 }
+
 
 export default App;
